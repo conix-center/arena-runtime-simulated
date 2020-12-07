e138f5a4f8a448055dd75b85a1bde25a6bd9e7da
Nuno Pereira <nampereira@gmail.com>
2020-12-07 18:42:07 -0500

    Updated py_run tests

--- tests/pyrun/test.sh
+++ tests/pyrun/test.sh
@@ -4,9 +4,13 @@
 #realm/proc/control {"object_id":"638eda2f-00a9-4a3d-b281-4ba10414db08","action":"create","type":"arts_req","data":{"type":"module","uuid":"aca2b0ba-429c-4186-ba7a-74dd7140bec7","name":"wiselab/robot-arm","parent":"{}","filename":"robot-arm.py","fileid":"na","filetype":"PY","env":"SCENE=room1 MQTTH=arena.andrew.cmu.edu REALM=realm","args":"","channels":[{"path":"/ch/room1","type":"pubsub","mode":"rw","params":{"topic":"realm/s/room1"}}],"wait_state":"false"}}
 #realm/proc/control/370c9341-8094-412c-a9bc-c809075f4453 {"object_id": "486ef8a2-2a63-404b-b515-ec99b5b95e97", "action": "create", "type": "arts_req", "data": {"type": "module", "uuid": "e5f4439e-5a02-4e5d-9d72-704d171d8949", "name": "wiselab/configure_scene_room1", "parent": {"uuid": "370c9341-8094-412c-a9bc-c809075f4453"}, "filename": "ground-scale.py", "fileid": "na", "filetype": "PY", "apis": "python:python3", "args": "", "env": "SCENE=room1 MQTTH=arena.andrew.cmu.edu REALM=realm", "channels": "[{'path': '/ch/room1', 'type': 'pubsub', 'mode': 'rw', 'params': {'topic': 'realm/s/room1'}}]"}}
 
+# load secrets
+export $(grep -v '^#' secrets.env | xargs)
+
 export __mqtt_srv="arena.andrew.cmu.edu"
+export __mqtt_prt=8883
 export __store_url="https://arena.andrew.cmu.edu/store"
-export __name="wiselab/robot-arm"
+export __name="wiselab/tmp/robot-arm"
 export __filename="robot-arm.py"
 export __fid=""
 export __pipe_stdin_stdout="True"
@@ -25,4 +29,4 @@ rm -fr ./tmp
 mkdir ./tmp
 echo $workdir
 
-../../runtimemngr/py_run.sh 
+../../runtimemngr/py_run.sh
